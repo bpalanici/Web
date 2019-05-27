a9164554bcd5935e08d07d1ab69234f410d3f42f
Dyian314 <48531023+Dyian314@users.noreply.github.com>
2019-05-27 20:21:50 +0300

    Configuration file

---
+++ Scripts/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '888430237192-cmme59v4ni192oeib38709m51qcg9jbi.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'kFWZ3UXSgvs6u7dk-BvEZi2a');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/Web/Scripts/redirect.php');
+
+?>
