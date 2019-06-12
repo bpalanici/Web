a1612286143767a9007d6542535538168fddbf5c
bpalanici <48147800+bpalanici@users.noreply.github.com>
2019-06-12 16:19:39 +0200

    little updates

--- Model/loginModel.php
+++ Model/loginModel.php
@@ -3,14 +3,14 @@
 	require_once('databaseConn.php');
 	session_start();
 
-	function IsRegistered($client_username) {	
+	function IsRegistered($client_mail) {	
 		$user = 'root';
 		$pass = '';
 		$db = 'web';
 		$conn = db::getConnection();
 
 		if ($stmt = $conn->prepare('SELECT count(*) FROM users where mail = ?')) {
-		    $stmt->bind_param("s", $client_username);
+		    $stmt->bind_param("s", $client_mail);
 		    $stmt->execute();
 		    $stmt->bind_result($rez);
 		    $stmt->fetch();
@@ -18,14 +18,23 @@
 			$stmt->close();
 			if ($rez == 1) {
 				if ($stmt2 = $conn->prepare('SELECT cfusername FROM users where mail = ?')) {
-				    $stmt2->bind_param("s", $client_username);
+				    $stmt2->bind_param("s", $client_mail);
 				    $stmt2->execute();
 				    $stmt2->bind_result($rez2);
 				    $stmt2->fetch();
 
 					$stmt2->close();
 					$_SESSION['cfusername'] = $rez2;
-					return $rez; 
+				}
+				else return 0;
+				if ($stmt2 = $conn->prepare('SELECT username FROM users where mail = ?')) {
+				    $stmt2->bind_param("s", $client_mail);
+				    $stmt2->execute();
+				    $stmt2->bind_result($rez2);
+				    $stmt2->fetch();
+
+					$stmt2->close();
+					$_SESSION['username'] = $rez2;
 				}
 				else return 0;
 			}
@@ -34,6 +43,6 @@
 		return 0;
 	}
 
-	//echo IsRegistered('a@dasda');
+	echo IsRegistered('a@dasda') . $_SESSION['username'];
 
 ?>
--- Model/modelEvents.php
+++ Model/modelEvents.php
@@ -51,26 +51,73 @@
               	</div></div>';
 			}
 		}	
-
+		
 		return $result ;
 	}
 
 	function getRecommendationsMeetup() {
-		$result = '';
+		$result = '';   
 
+		error_reporting(E_ERROR | E_PARSE);
 		$dom = new domDocument; 
-   
-   /*** load the html into the object ***/ 
 
 		$htmlContent = file_get_contents('https://www.meetup.com/find/events/tech/?allMeetups=false&radius=100&userFreeform=Iasi%2C+Romania&mcId=c1033319&mcName=Iasi%2C+RO');
 		$dom->loadHTML($htmlContent); 
 
-		$xpath = new DOMXpath($dom);/*
+		$xpath = new DOMXpath($dom);
+		$xpath2 = new DOMXpath($dom);
+		$date = array();
+		$eventsThatDate = array();
+		$group = array();
+		$name = array();
 
-	   foreach ($xpath->query('//a/span[contains(@itemprop, \'name\')]') as $key => $value) {
-	   	echo $value->nodeValue . '</br>';
-	   }*/
-	   	$i = 1;
+		$i = 1;
+		$index = 0;
+		foreach ($xpath->query('//div[contains(@class, \'unit size5of7 \')]/ul/li') as $key => $value) {
+		  if ($i == 1) {
+		  	$date[$index] = $value->nodeValue . ' ';
+		     $index++;
+		  }
+		  else {
+		     $eventsThatDate[$index - 1] = count(explode('going', $value->nodeValue)) - 1;
+		  }
+		  $i = 3 - $i;
+		}
+
+		$i = 0;
+		foreach ($xpath->query('//a/span[contains(@itemprop, \'name\')]') as $key => $value) {
+		 if ($i % 2 == 0) 
+		    $group[$i / 2] .= $value->nodeValue . ' ';
+		 else 
+		    $name[($i - 1) / 2] .= $value->nodeValue . ' ';
+		 $i++;
+		}
+
+		$i = 0;
+		$offset = 0;
+		while ($i < count($date)) {
+		  //echo $date[$i] . '</br>';
+		  $i1 = 0;
+		  while ($i1 < $eventsThatDate[$i])  {
+		     $result .= '<div class="card"><div class=flexInsideCard>
+	                <a class="card-title">' . $date[$i] . '</br> Group : ' . $group[$i + $i1 + $offset] . '</br></br>'. $name[$i + $i1 + $offset] . '</br></br>Difficulty : Hard</a>
+	            	<button class="buttonCard"> Apply </button>
+	          	</div></div>';
+		     $i1++;
+		     $offset++;
+		  }
+		  $offset--;
+		  $i++;
+		}
+/*
+		while ($i < count($date)) {
+		  $result .= '<div class="card"><div class=flexInsideCard>
+	                <a class="card-title">' . $date[$i] . ' ' . $group[$i] . ' '. $name[$i] . ' </br>Difficulty : Hard</a>
+	            	<button class="buttonCard"> Apply </button>
+	          	</div></div>';
+		  $i++;
+		}*/
+/*
 		foreach ($xpath->query('//div[contains(@class, \'unit size5of7 \')]/ul/li') as $key => $value) {
 			if ($i == 1)
 				$result .= '<div class="card"><div class=flexInsideCard>
@@ -85,7 +132,7 @@
 	          	$i = 1;
 	        }
 			
-		}	
+		}*/	
 
 		return $result ;
 	}
